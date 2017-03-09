86c0b43e891091f7eea0213706525b2042c8ba9c
myamagu1 <myamagu1@lion.lmu.edu>
2017-03-09 15:14:47 -0800

    Defined my firebaseConfig constant

--- Reppy/src/app/app.module.ts
+++ Reppy/src/app/app.module.ts
@@ -1,29 +1,37 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAMDhxU69glVb2qkr_6JzsCprh9SXDdsFk",
+    authDomain: "reppy-b9a64.firebaseapp.com",
+    databaseURL: "https://reppy-b9a64.firebaseio.com",
+    storageBucket: "reppy-b9a64.appspot.com",
+    messagingSenderId: "721607679516"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
