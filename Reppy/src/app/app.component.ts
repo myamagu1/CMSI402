0d5a8133562911096c6d12cd12df8395ff7fe3f5
myamagu1 <myamagu1@lion.lmu.edu>
2017-03-11 21:41:29 -0800

    rootPage = LoginPage

--- Reppy/src/app/app.component.ts
+++ Reppy/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 import * as firebase from 'firebase';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  // public rootPage: any = null;
+  rootPage = LoginPage;
 
   constructor(platform: Platform) {
 
@@ -22,6 +24,14 @@ export class MyApp {
       messagingSenderId: "721607679516"
     };
     firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            this.rootPage = TabsPage;
+        } else {
+            this.rootPage = LoginPage;
+        }
+    });
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
