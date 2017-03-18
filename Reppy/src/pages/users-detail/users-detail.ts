e60e4853afad411668a8df2f0823efe9d72a1667
myamagu1 <myamagu1@lion.lmu.edu>
2017-03-17 17:08:46 -0700

    Logout feature working

--- Reppy/src/pages/users-detail/users-detail.ts
+++ Reppy/src/pages/users-detail/users-detail.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { UsersService } from '../../providers/users-service/users-service';
+import { LoginPage } from '../login/login';
+
+
 
 /*
   Generated class for the UsersDetail page.
@@ -9,11 +13,19 @@ import { NavController, NavParams } from 'ionic-angular';
 */
 @Component({
   selector: 'page-users-detail',
-  templateUrl: 'users-detail.html'
+  templateUrl: 'users-detail.html',
+  providers: [UsersService]
 })
 export class UsersDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private usersService: UsersService ) {}
+
+  logUserOut() {
+    //   this.userService.logoutUser();
+    this.usersService.logoutUser().then(() => {
+        this.navCtrl.setRoot(LoginPage);
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UsersDetailPage');
