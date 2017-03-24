77c0bbf3d574ffcb20fa17c0879c4e7c595a3c7f
myamagu1 <myamagu1@lion.lmu.edu>
2017-03-24 15:30:18 -0700

    Working on addNewPost()

--- Reppy/src/pages/post-add/post-add.ts
+++ Reppy/src/pages/post-add/post-add.ts
@@ -1,22 +1,75 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, ViewController,LoadingController,AlertController } from 'ionic-angular';
+import { PostsService } from '../../providers/posts-service/posts-service';
+import * as firebase from 'firebase';
 
 /*
-  Generated class for the PostAdd page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
+Generated class for the PostAddPage page.
+See http://ionicframework.com/docs/v2/components/#navigation for more info on
+Ionic pages and navigation.
 */
+
 @Component({
-  selector: 'page-post-add',
-  templateUrl: 'post-add.html'
+    selector: 'page-post-add',
+    templateUrl: 'post-add.html',
+    providers: [PostsService]
 })
+
 export class PostAddPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    private postTitle :any;
+    private postBody :any;
+    private userId :any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PostAddPage');
-  }
 
+    constructor(private navCtrl: NavController, private loadingCtrl: LoadingController,private viewCtrl: ViewController, private postsService: PostsService, private alertCtrl: AlertController ) {
+        //user id of current logged in user
+        this.userId = firebase.auth().currentUser.uid;
+    }
+
+    addNewPost(){
+
+        //add preloader
+        let loading = this.loadingCtrl.create({
+            dismissOnPageChange: true,
+            content: 'Reseting your password..'
+        });
+        loading.present();
+
+        //call the service
+        this.postsService.createPostService(this.userId, this.postBody).then(() => {
+
+            //clear the fields
+            this.postBody = "";
+
+            //add toast
+            loading.dismiss().then(() => {
+                //show pop up
+                let alert = this.alertCtrl.create({
+                    title: 'Done!',
+                    subTitle: 'Post successful',
+                    buttons: ['OK']
+                });
+                alert.present();
+            })
+
+            //close the popup
+            this.viewCtrl.dismiss();
+
+        }, error => {
+            //show pop up
+            loading.dismiss().then(() => {
+                let alert = this.alertCtrl.create({
+                    title: 'Error adding new post',
+                    subTitle: error.message,
+                    buttons: ['OK']
+                });
+                alert.present();
+            })
+        });
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad PostsPage');
+    }
 }
