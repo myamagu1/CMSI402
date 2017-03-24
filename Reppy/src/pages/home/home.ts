562cd92a2be93c6510f5280d1902c375062c4aca
myamagu1 <myamagu1@lion.lmu.edu>
2017-03-24 15:03:07 -0700

    Added functions for creating a post

--- Reppy/src/pages/home/home.ts
+++ Reppy/src/pages/home/home.ts
@@ -1,19 +1,71 @@
 import { Component } from '@angular/core';
-import { UsersDetailPage } from '../users-detail/users-detail';
 import { NavController } from 'ionic-angular';
+import { UsersDetailPage } from '../users-detail/users-detail';
+import { UsersService } from '../../providers/users-service/users-service';
+import { PostsService } from '../../providers/posts-service/posts-service';
+import { PostAddPage } from '../post-add/post-add';
+import * as firebase from 'firebase';
 
 @Component({
     selector: 'page-home',
-    templateUrl: 'home.html'
+    templateUrl: 'home.html',
+    providers: [PostsService, UsersService]
 })
+
 export class HomePage {
 
-    constructor(public navCtrl: NavController) {
+    private userPostsLists= [];
+    private userProfileLists: any;
+    private userDisplayName: any;
+    private userEmail: any;
+    private userPhoto: any;
+    private userId: any;
 
+    constructor(public navCtrl: NavController, private postsService: PostsService, private usersService: UsersService) {
+
+        this.userProfileLists = firebase.database().ref('users');
+        this.userId = firebase.auth().currentUser.uid;
+        //get list of posts on page init
+        this.listPosts();
     }
 
-    redirectToUserDetailPage() {
-        this.navCtrl.push(UsersDetailPage)
+    redirectToUserDetailPage(){
+
+        //redirect here
+        this.navCtrl.push(UsersDetailPage);
     }
 
+    redirectToPostAddPage(){
+        //redirect here
+        this.navCtrl.push(PostAddPage);
+    }
+
+    listPosts(){
+        var that = this;
+        this.postsService.listPostService().then(snapshot => {
+            //empty this array first to avoid duplication of content when value changes in the database
+            //so every time there is a change in the database, empty the array, fetch fresh data from db
+            //this is because we are fetching data with on('value') inside listPostService()
+
+            that.userPostsLists.length = 0;
+
+            snapshot.forEach(function (childSnapshot) {
+                var data = childSnapshot.val();
+                data['key'] = childSnapshot.key;
+                that.userPostsLists.push(data);
+
+
+                console.log("post details: "+that.userPostsLists);
+                //get the user's detail
+                that.usersService.viewUser(that.userId).then(snapshotUser=> {
+                    that.userDisplayName = snapshotUser.val().username;
+                    that.userEmail = snapshotUser.val().email;
+                    that.userPhoto = snapshotUser.val().photo;
+
+                    //check the console section of your browser inspect element
+                    console.log( "user details: "+ snapshotUser.val() );
+                })
+            });
+        });
+    }
 }
--- Reppy/src/providers/posts-service/posts-service.ts
+++ Reppy/src/providers/posts-service/posts-service.ts
@@ -1,18 +1,63 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import * as firebase from 'firebase';
 
 /*
-  Generated class for the PostsService provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
+Generated class for the PostsService provider.
+See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+for more info on providers and Angular 2 DI.
 */
 @Injectable()
 export class PostsService {
 
-  constructor(public http: Http) {
-    console.log('Hello PostsService Provider');
-  }
+    private data: any;
+    private userNode: any;
+    private fireRef: any;
+    private postsNode: any;
+    private usersPostsNode: any;
 
+    constructor(private http: Http) {
+        this.userNode = firebase.database().ref('users');
+        this.postsNode = firebase.database().ref('posts');
+        this.usersPostsNode = firebase.database().ref('user-posts');
+        this.fireRef = firebase.database().ref();
+
+    }
+
+    //view a certain Post
+    viewPostService(postId:any){
+        var userRef = this.postsNode.child(postId);
+        return userRef.once('value');
+    }
+
+    //view all posts made by this userId
+    viewUsersPostsService(userId: any){
+        var userRef = this.usersPostsNode.child(userId);
+        return userRef.on('value');
+    }
+
+    listPostService(){
+        return this.postsNode.once('value');
+    }
+
+    createPostService(userId: any, postBody: any){
+        // A post entry.
+        var postData = {
+            uid: userId,
+            body: postBody
+        };
+
+        // Get a key for a new Post.
+        var newPostKey = this.postsNode.push().key;
+
+        // Write the new post's data simultaneously in the posts list and the user's post list.
+        var updatePath = {};
+        updatePath['/posts/' + newPostKey] = postData;
+        updatePath['/user-posts/' +userId+"/"+ newPostKey] = postData;
+
+        //update both tables simultaneously
+        return this.fireRef.update(updatePath);
+
+    }
 }
