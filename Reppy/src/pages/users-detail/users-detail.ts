d69909b47bc34e24e3a165b3ba1bc6b1f46de3b7
myamagu1 <myamagu1@lion.lmu.edu>
2017-04-02 14:45:40 -0700

    Redesigning UserDetailsPage front-end

--- Reppy/src/pages/users-detail/users-detail.ts
+++ Reppy/src/pages/users-detail/users-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { UsersService } from '../../providers/users-service/users-service';
+import { PostsService } from '../../providers/posts-service/posts-service';
 import { LoginPage } from '../login/login';
 import * as firebase from 'firebase';
 
@@ -14,17 +15,28 @@ Ionic pages and navigation.
 @Component({
     selector: 'page-users-detail',
     templateUrl: 'users-detail.html',
-    providers: [UsersService]
+    providers: [UsersService, PostsService]
 })
 export class UsersDetailPage {
 
-    private userPhotoUrl: any;
-    private userDislplayName: any;
+    private userPostsLists= [];
+    private userProfileLists: any;
+    private userDisplayName: any;
+    private userEmail: any;
+    private userPhoto: any;
+    private userId: any;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private usersService: UsersService ) {
+    private userPhotoUrl: any;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private usersService: UsersService, private postsService: PostsService ) {
         //current user id
         var myUserId = firebase.auth().currentUser.uid;
         this.displayUser(myUserId);
+
+        this.userProfileLists = firebase.database().ref('users');
+        this.userId = firebase.auth().currentUser.uid;
+        //get list of posts on page init
+        this.listPosts();
     }
 
     displayUser(theUserId){
@@ -35,7 +47,7 @@ export class UsersDetailPage {
 
             //get user photo
             that.userPhotoUrl = snapshot.val().photo; //get user photo
-            that.userDislplayName= snapshot.val().username;
+            that.userDisplayName= snapshot.val().username;
         })
     }
 
@@ -43,6 +55,35 @@ export class UsersDetailPage {
         //   this.userService.logoutUser();
         this.usersService.logoutUser().then(() => {
             this.navCtrl.setRoot(LoginPage);
+        });
+    }
+
+    listPosts(){
+        var that = this;
+        this.postsService.listPostService().then(snapshot => {
+            //empty this array first to avoid duplication of content when value changes in the database
+            //so every time there is a change in the database, empty the array, fetch fresh data from db
+            //this is because we are fetching data with on('value') inside listPostService()
+
+            that.userPostsLists.length = 0;
+
+            snapshot.forEach(function (childSnapshot) {
+                var data = childSnapshot.val();
+                data['key'] = childSnapshot.key;
+                that.userPostsLists.push(data);
+
+
+                console.log("post details: "+that.userPostsLists);
+                //get the user's detail
+                that.usersService.viewUser(that.userId).then(snapshotUser=> {
+                    that.userDisplayName = snapshotUser.val().username;
+                    that.userEmail = snapshotUser.val().email;
+                    that.userPhoto = snapshotUser.val().photo;
+
+                    //check the console section of your browser inspect element
+                    console.log( "user details: "+ snapshotUser.val() );
+                })
+            });
         });
     }
 
